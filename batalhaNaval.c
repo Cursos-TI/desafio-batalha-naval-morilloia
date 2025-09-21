a53dc618698894ad0155d2d1b3678ad4f0f1028f
Israel Morillo <israelmorilloosorio11@gmail.com>
2025-09-21 22:57:15 +0000

    Fiz o desafio nível novato do posicionamento dos navios e a exibição do tabuleiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -10,6 +10,70 @@ int main() {
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
+    int tabuleiro[10][10] = {
+        {0,0,0,0,0,0,0,0,0,0}, 
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+    };
+    int indexVertical, indexHorizontal, i;
+    printf("Tabuleiro Inicial:\n");
+   
+    // Display do navio vertical
+    indexVertical = 2; // Coluna fixa
+    printf("Navio Vertical (Coluna %d):\n", indexVertical);
+    for (i = 2; i < 5; i++){
+        if (tabuleiro[i][indexVertical] != 0) {
+            printf("A posição (%d, %d) já foi ocupada\n",i, indexVertical);
+            return 1; // Sai do programa com código de erro
+        } else{
+        tabuleiro[i][indexVertical] = 3; // Marca o navio no tabuleiro
+        printf("Parte do navio na posicao: (%d, %d)\n", i, indexVertical); //Printa as posições do navio
+        }
+    }
+
+    // Display do navio horizontal
+    indexHorizontal = 8;
+    printf("Navio Horizontal (Linha %d):\n", indexHorizontal);
+    for( i = 5; i < 8; i++){
+        if (tabuleiro[indexHorizontal][i] != 0) 
+        {
+            printf("A posição (%d, %d) já foi ocupada\n",indexHorizontal, i);
+            return 1; // Sai do programa com código de erro
+        } else {
+            tabuleiro[indexHorizontal][i] = 3; // Marca o navio no tabuleiro
+            printf("Parte do navio na posicao: (%d, %d)\n", indexHorizontal, i);
+        }
+    }
+    // Display do tabuleiro após posicionar os navios
+    printf("Tabuleiro naval:\n");
+
+     for (i = 0; i < 10; i++) // Loop para linhas
+    {   
+        for (int j = 0; j < 10; j++) // Loop para colunas
+        {
+            printf("%d ", tabuleiro[i][j]); 
+        }
+        printf("\n");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
